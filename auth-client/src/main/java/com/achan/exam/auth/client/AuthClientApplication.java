fad36dfb0621b6d5895d74f4ce0aebedb82b9c6b
achan <yyx1310059081@gmail.com>
2020-03-16 16:00:33 +0800

    增加logout接口

--- auth-client/src/main/java/com/achan/exam/auth/client/AuthClientApplication.java
+++ auth-client/src/main/java/com/achan/exam/auth/client/AuthClientApplication.java
@@ -1,11 +1,13 @@
 package com.achan.exam.auth.client;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author Achan
  */
 @SpringBootApplication
+@ComponentScan("com.achan.exam.util")
 public class AuthClientApplication {
 
 }
--- auth-client/src/main/java/com/achan/exam/auth/client/controller/AuthController.java
+++ auth-client/src/main/java/com/achan/exam/auth/client/controller/AuthController.java
@@ -3,15 +3,18 @@ package com.achan.exam.auth.client.controller;
 import cn.hutool.http.HttpUtil;
 import com.achan.exam.auth.client.bean.param.UserParam;
 import com.achan.exam.common.annotation.BaseResponse;
+import com.achan.exam.util.redis.SimpleStringRedisUtil;
 import com.sun.istack.NotNull;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.exceptions.InvalidGrantException;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -36,6 +39,8 @@ public class AuthController {
     private String clientSecret;
     @Value("${security.oauth2.client.access-token-uri}")
     private String tokenUri;
+    @Autowired
+    private SimpleStringRedisUtil redisUtil;
 
     @PostMapping("/login")
     @ApiOperation("登录")
@@ -46,9 +51,17 @@ public class AuthController {
         params.put("grant_type", "password");
         params.put("client_id", clientId);
         params.put("client_secret", clientSecret);
+        return getOAuth2Token(params);
+    }
+
+    private OAuth2AccessToken getOAuth2Token(Map<String, Object> params) throws IOException {
         String result = HttpUtil.post(tokenUri, params, 1000 * 30);
         ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.readValue(result, OAuth2AccessToken.class);
+        OAuth2AccessToken resultObject = objectMapper.readValue(result, OAuth2AccessToken.class);
+        if (resultObject.getValue() == null) {
+            throw objectMapper.readValue(result, InvalidGrantException.class);
+        }
+        return resultObject;
     }
 
     @GetMapping("/me")
@@ -66,8 +79,19 @@ public class AuthController {
         params.put("client_id", clientId);
         params.put("client_secret", clientSecret);
         params.put("refresh_token", token);
-        String result = HttpUtil.post(tokenUri, params, 1000 * 30);
-        ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.readValue(result, OAuth2AccessToken.class);
+        return getOAuth2Token(params);
+    }
+
+    @RequestMapping(value = "/logout", method = {RequestMethod.DELETE, RequestMethod.POST})
+    @ApiOperation("退出登录")
+    public void logout(@RequestParam(name = "access-token") @Validated @NotBlank String accessToken,
+                       @RequestParam(name = "refresh-token") @Validated @NotBlank String refreshToken) {
+        // TODO: 这里只是把能看明白是干啥的删掉了，没有删全
+        redisUtil.delete("access:" + accessToken);
+        redisUtil.delete("auth:" + accessToken);
+        redisUtil.delete("access_to_refresh:" + accessToken);
+        redisUtil.delete("refresh:" + refreshToken);
+        redisUtil.delete("refresh_auth:" + refreshToken);
+        redisUtil.delete("refresh_to_access:" + refreshToken);
     }
 }
